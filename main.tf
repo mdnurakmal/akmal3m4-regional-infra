b10250878bd1c581aae2494f45a034a5d067c6d9
Nur Akmal <mdnurakmal@gmail.com>
2021-12-18 14:19:25 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,18 +17,28 @@ provider "google" {
   region      = var.region
 }
 
+
 resource "google_compute_backend_service" "game-server-backend-service" {
   provider = google-beta
+  project = var.project_id
   name                            = "game-server-backend-service-${var.multiregion}"
   enable_cdn                      = true
-  timeout_sec                     = 10
   connection_draining_timeout_sec = 10
 
   backend {
-    group = google_compute_region_network_endpoint_group.game-server-neg.id
+    group = game-server-asia-neg
   }
 
+}
+
+resource "google_compute_backend_service" "game-client-backend-service" {
+  provider = google-beta
+  project = var.project_id
+  name                            = "game-client-backend-service-${var.multiregion}"
+  enable_cdn                      = true
+  connection_draining_timeout_sec = 10
+
   backend {
-    group = google_compute_region_network_endpoint_group.game-client-neg.id
+    group = game-client-asia-neg
   }
 }
